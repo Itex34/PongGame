20bed428abe554ee8ee23af4b776500cb9095405
Itex34 <83979354+Itex34@users.noreply.github.com>
2024-10-31 11:34:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,16 @@
 #include "SDL2/SDL.h"
 #include "Game.hpp"
 #include "Paddle.hpp"
+#include "NPCPaddle.hpp"
+#include "Ball.hpp"
 #include <iostream>
+#include <random>
 Game game;
+
+Paddle paddle;
+NPCPaddle npcPaddle;
+Ball ball;
+
 int main(int argc, char* argv[]) {
 
     const float targetFPS = 60.0f;
@@ -13,12 +21,25 @@ int main(int argc, char* argv[]) {
     int windowWidth = 1300;
     int windowHeight = 700;
 
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    std::uniform_int_distribution<> ballY(50, 600);
+
     game.init("GameWindow", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight, false);
 
-    Paddle paddle;
+
 
     if (!paddle.loadTexture(game.getRenderer(), "paddle.png")) {
         std::cerr << "Failed to load paddle texture" << std::endl;
+        return -1;
+    }
+    if (!npcPaddle.loadTexture(game.getRenderer(), "paddle.png")) {
+        std::cerr << "Failed to load paddle texture" << std::endl;
+        return -1;
+    }
+    if (!ball.loadTexture(game.getRenderer(), "ball.png")) {
+        std::cerr << "Failed to load ball texture" << std::endl;
         return -1;
     }
 
@@ -31,11 +52,16 @@ int main(int argc, char* argv[]) {
         game.update();
 
         paddle.update();
-
-        SDL_RenderPresent(game.getRenderer());
+        npcPaddle.update();
+        ball.update(game.getRenderer(), windowWidth, windowHeight, ballY(gen));
 
         game.render(windowHeight, windowWidth);
-        paddle.render(game.getRenderer(), 20, windowHeight / 2);
+
+        paddle.render(game.getRenderer(), windowWidth, windowHeight / 2);
+        npcPaddle.render(game.getRenderer(), windowWidth, windowHeight / 2);
+        ball.render(game.getRenderer(), windowWidth, windowHeight / 2);
+
+        SDL_RenderPresent(game.getRenderer());
 
         Uint32 frameTicks = SDL_GetTicks() - currentTicks;
         if (frameTicks < maxFrameTime * 1000.0f) {
